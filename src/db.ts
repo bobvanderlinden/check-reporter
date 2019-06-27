422af61ef89786b6bd1eafc5ae4516a2aafde135
Bob van der Linden <bobvanderlinden@gmail.com>
2019-06-27 09:55:21 +0200

    initial working version

---
+++ src/db.ts
@@ -0,0 +1,19 @@
+import { Client } from 'pg'
+
+export async function connect() {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  })
+  await client.connect()
+  return client
+}
+
+export async function use(fn: (client: Client) => Promise<void>) {
+  const client = await connect()
+  try {
+    await fn(client)
+  } finally {
+    await client.end()
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,72 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { use } from './db'
+
+class HttpError extends Error {
+  public status: number
+  constructor(data: {status: number, message: string}) {
+    super(data.message)
+    this.status = data.status
+  }
+
+  toString(): string {
+    return `[HttpError status=${this.status} message=${this.message}]`
+  }
+}
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
+  app.on('check_suite.requested', async (context) => {
+    const { github, payload } = context
+    const { head_sha, status, conclusion } = payload
+    const repo = payload.repository.name
+    const owner = payload.repository.owner.login
+    await github.checks.createSuite({
+      head_sha,
+      owner,
+      repo
+    })
   })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  const router = app.route('/api')
+
+  router.use(require('body-parser').json())
+
+  router.post('/push', (req: any, res: any) => {
+    (async () => {
+      const [authenticationMethod, authenticationToken] = req.headers.authorization.split(' ', 2)
+      if (authenticationMethod !== 'Bearer') {
+        throw new HttpError({
+          status: 400,
+          message: 'Invalid authentication header'
+        })
+      }
+      await use(async (db) => {
+        const { rows } = await db.query('select installation_id, owner, repo from tokens where token = $1', [authenticationToken])
+        if (rows.length === 0) {
+          throw new HttpError({
+            status: 401,
+            message: 'Unrecognized token'
+          })
+        }
+        if (rows.length > 1) {
+          throw new Error('Multiple rows for token')
+        }
+        const { installation_id, owner, repo } = rows[0]
+        const github = await app.auth(installation_id, app.log)
+        await github.checks.create({
+          ...req.body,
+          owner,
+          repo
+        })
+      })
+    })().then(() => {
+      res.end()
+    }).catch(err => {
+      app.log('Error handling push request', err)
+      if (err instanceof HttpError) {
+        res.status(err.status).end()
+      } else {
+        res.status(500).end()
+      }
+    })
+  })
 }
