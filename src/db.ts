9cdbf6a9f7ac3308098291823620ea07beba7c19
Bob van der Linden <bobvanderlinden@gmail.com>
2021-08-19 16:21:20 +0200

    apply prettier

--- src/db.ts
+++ src/db.ts
@@ -1,19 +1,19 @@
-import { Client } from 'pg'
+import { Client } from "pg";
 
 export async function connect() {
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
-    ssl: true
-  })
-  await client.connect()
-  return client
+    ssl: true,
+  });
+  await client.connect();
+  return client;
 }
 
 export async function use(fn: (client: Client) => Promise<void>) {
-  const client = await connect()
+  const client = await connect();
   try {
-    await fn(client)
+    await fn(client);
   } finally {
-    await client.end()
+    await client.end();
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,66 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
-import { use } from './db'
+import { Application } from "probot"; // eslint-disable-line no-unused-vars
+import { use } from "./db";
 
 class HttpError extends Error {
-  public status: number
-  constructor(data: {status: number, message: string}) {
-    super(data.message)
-    this.status = data.status
+  public status: number;
+  constructor(data: { status: number; message: string }) {
+    super(data.message);
+    this.status = data.status;
   }
 
   toString(): string {
-    return `[HttpError status=${this.status} message=${this.message}]`
+    return `[HttpError status=${this.status} message=${this.message}]`;
   }
 }
 
 export = (app: Application) => {
-  const router = app.route('/api')
+  const router = app.route("/api");
 
-  router.use(require('body-parser').json())
+  router.use(require("body-parser").json());
 
-  router.post('/push', (req: any, res: any) => {
+  router.post("/push", (req: any, res: any) => {
     (async () => {
-      const [authenticationMethod, authenticationToken] = req.headers.authorization.split(' ', 2)
-      if (authenticationMethod !== 'Bearer') {
+      const [authenticationMethod, authenticationToken] =
+        req.headers.authorization.split(" ", 2);
+      if (authenticationMethod !== "Bearer") {
         throw new HttpError({
           status: 400,
-          message: 'Invalid authentication header'
-        })
+          message: "Invalid authentication header",
+        });
       }
       await use(async (db) => {
-        const { rows } = await db.query('select installation_id, owner, repo from tokens where token = $1', [authenticationToken])
+        const { rows } = await db.query(
+          "select installation_id, owner, repo from tokens where token = $1",
+          [authenticationToken]
+        );
         if (rows.length === 0) {
           throw new HttpError({
             status: 401,
-            message: 'Unrecognized token'
-          })
+            message: "Unrecognized token",
+          });
         }
         if (rows.length > 1) {
-          throw new Error('Multiple rows for token')
+          throw new Error("Multiple rows for token");
         }
-        const { installation_id, owner, repo } = rows[0]
-        const github = await app.auth(installation_id, app.log)
+        const { installation_id, owner, repo } = rows[0];
+        const github = await app.auth(installation_id, app.log);
         await github.checks.create({
           ...req.body,
           owner,
-          repo
-        })
+          repo,
+        });
+      });
+    })()
+      .then(() => {
+        res.end();
       })
-    })().then(() => {
-      res.end()
-    }).catch(err => {
-      app.log('Error handling push request', err)
-      if (err instanceof HttpError) {
-        res.status(err.status).end()
-      } else {
-        res.status(500).end()
-      }
-    })
-  })
-}
+      .catch((err) => {
+        app.log("Error handling push request", err);
+        if (err instanceof HttpError) {
+          res.status(err.status).end();
+        } else {
+          res.status(500).end();
+        }
+      });
+  });
+};
