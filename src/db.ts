6f7b4e29f65ad9d3870126c60cc0b44145021007
Bob van der Linden <bobvanderlinden@gmail.com>
2021-08-20 08:54:44 +0200

    allow unauthorized db connections

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import { Client } from "pg";
 export async function connect() {
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    ssl: { rejectUnauthorized: false },
   });
   await client.connect();
   return client;
